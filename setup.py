b16a5afe1ef2dafed1fe988c637f0022eeeff6c0
Louis Sobel <sobel@lsmbp.local>
2012-07-18 11:04:43 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+	name='python-termstyle',
+	version='0.1',
+	description='Terminal Styling',
+	url='',
+	author='Louis Sobel',
+	py_modules=["termstyle"]
+)
